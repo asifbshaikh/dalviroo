1eee071350077d945700aa9f1684bb39eb52e536
ASIF SHAIKH <asif.shaikh@techbulls.com>
2018-05-14 18:47:03 +0530

    added form

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180514055027) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "product_id"
     t.integer "quantity"
---
+++ public/mainCtr.js
@@ -0,0 +1,9 @@
+
+
+angular.module('dalviroo', []).controller('MainCtrl', function($scope,$http) {
+
+    console.log("In Dashboard Controller")
+ 	$scope.firstName = "John";
+    $scope.lastName = "Doe";
+    
+});
